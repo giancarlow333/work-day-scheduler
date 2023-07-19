b4c65ca1acb45ec42687aee2faf2278e32c4d0dd
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-18 18:54:44 -0700

    Add current date to header

--- Develop/script.js
+++ Develop/script.js
@@ -20,4 +20,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var toDay = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(toDay);
 });
